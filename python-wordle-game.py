28ae5d350edba757e1203da72af10bda7faaa1df
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-06-08 13:44:34 -0400

    added full word list from wordle

--- python-wordle-game.py
+++ python-wordle-game.py
@@ -1,13 +1,12 @@
 from random import choice
 
-words = ['puree', 'grape', 'fishy', 'start', 'large', 'small', 'extra', 'tacos', 'plump', 'wiley', 'paled', 'story']
-answer = choice(words)
 board = [["_","_","_","_","_"] for i in range(6)]
 row = 0
 reset = '\033[0m'
 grey = '\033[1;30m'
 green = '\033[1;32m'
 yellow = '\033[1;33m'
+purple = "\033[0;35m"
 
 def space_out(x):
     return ' '.join(x)
@@ -18,6 +17,19 @@ def show_board():
 
 def set_color(text, color):
     return f'{color}{text}{reset}'
+
+def loadWords():
+    # print("Loading word list from file...")
+    inFile = open('word-list.txt', 'r')
+    wordList = []
+    for line in inFile:
+        wordList.append(line.strip().lower())
+    # print("  ", len(wordList), "words loaded.")
+    return wordList
+
+# words = ['puree', 'grape', 'fishy', 'start', 'large', 'small', 'extra', 'tacos', 'plump', 'wiley', 'paled', 'story']
+word_list = loadWords()
+answer = choice(word_list)
         
 show_board()
 
@@ -29,24 +41,28 @@ while row < 6:
         continue
 
     # check if entered word is a valid word
+
     correct_count = 0
     temp_answer = list(answer)
     for index, letter in enumerate(guess):
         if letter == answer[index]:
-            board[row][index] = set_color(letter, green)
+            board[row][index] = set_color(letter.upper(), green)
             temp_answer.remove(letter)
             correct_count += 1
         elif temp_answer.count(letter) > 0:
-            board[row][index] = set_color(letter, yellow)
+            board[row][index] = set_color(letter.upper(), yellow)
             temp_answer.remove(letter)
         else:
-            board[row][index] = set_color(letter, grey)
+            board[row][index] = set_color(letter.upper(), grey)
+    
+    show_board()
     
     if correct_count == 5:
         print(f'You win! Tries: {row + 1}')
         break
 
-    show_board()
-    row += 1
+    if row == 5:
+        print(f'Game over. The word was {set_color(answer.upper(), purple)}.')
+        break
 
-print(f'Game over. The word was {answer}')
+    row += 1
